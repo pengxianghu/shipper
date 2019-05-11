0c72ebf7d64d60fd0887196808d9927a2c97fa32
hpx <m15257990085@163.com>
2019-05-11 14:12:32 +0800

    modify user-cli

--- user-cli/cli.go
+++ user-cli/cli.go
@@ -2,85 +2,50 @@ package main
 
 import (
 	"log"
-	"os"
 
 	pb "github.com/pengxianghu/shipper/user-service/proto/user"
 	microclient "github.com/micro/go-micro/client"
-	"github.com/micro/go-micro/cmd"
 	"golang.org/x/net/context"
-	"github.com/micro/cli"
 	"github.com/micro/go-micro"
 )
 
 
 func main() {
 
-	cmd.Init()
+	srv := micro.NewService(
 
-	// Create new greeter client
-	client := pb.NewUserServiceClient("go.micro.srv.user", microclient.DefaultClient)
+        micro.Name("go.micro.srv.user-cli"),
+        micro.Version("latest"),
+    )
 
-	service := micro.NewService(
-		micro.Flags(
-			cli.StringFlag{
-				Name:  "name",
-				Usage: "You full name",
-			},
-			cli.StringFlag{
-				Name:  "email",
-				Usage: "Your email",
-			},
-			cli.StringFlag{
-				Name:  "password",
-				Usage: "Your password",
-			},
-			cli.StringFlag{
-				Name: "company",
-				Usage: "Your company",
-			},
-		),
-	)
+    // Init will parse the command line flags.
+    srv.Init()
 
-	service.Init(
+    client := pb.NewUserServiceClient("go.micro.srv.user", microclient.DefaultClient)
 
-		micro.Action(func(c *cli.Context) {
+    name := "hupx"
+    email := "hpx@163.com"
+    password := "p"
+    company := "ali"
 
-			// name := c.String("name")
-			// email := c.String("email")
-			// password := c.String("password")
-            // company := c.String("company")
-            
-            name := "hupxla"
-			email := "1525799@163.com"
-			password := "hupx"
-			company := "jd"
+    r, err := client.Create(context.TODO(), &pb.User{
+        Name:     name,
+        Email:    email,
+        Password: password,
+        Company:  company,
+    })
+    if err != nil {
+        log.Fatalf("Could not create: %v", err)
+    }
+    log.Printf("Created: %s", r.User.Id)
 
-			r, err := client.Create(context.TODO(), &pb.User{
-				Name: name,
-				Email: email,
-				Password: password,
-				Company: company,
-			})
-			if err != nil {
-				log.Fatalf("Could not create: %v", err)
-			}
-			log.Printf("Created: %t", r.User.Id)
-
-			getAll, err := client.GetAll(context.Background(), &pb.Request{})
-			if err != nil {
-				log.Fatalf("Could not list users: %v", err)
-			}
-			for _, v := range getAll.Users {
-				log.Println(v)
-			}
-
-			// let's just exit because
-			os.Exit(0)
-		}),
-	)
-
-	// Run the server
-	if err := service.Run(); err != nil {
-		log.Println(err)
+    getAll, err := client.GetAll(context.Background(), &pb.Request{})
+    if err != nil {
+        log.Fatalf("Could not list users: %v", err)
+    }
+    for _, v := range getAll.Users {
+        log.Println(v)
 	}
+	
+
 }
